05e1a800a9c7013eff41b9ccee1f99c7078da11c
Zoja Mancek Pali <s2095338@ed.ac.uk>
2024-03-27 11:42:12 +0000

    canopy positions changes + attempting pca

--- Scripts/Dissertation.R
+++ Scripts/Dissertation.R
@@ -81,7 +81,7 @@ cn_trees$age <- as.numeric(cn_trees$age)
 cn_nns <- cn_trees %>% 
   filter(type %in% c('Native', 'Naturalised', "Invasive")) %>% #excluding the alien group for initial analysis
   mutate(canopy_pos = recode(canopy_pos, 
-                             "L" = "Lower",
+                             "M" = "Lower",
                              "U" = "Upper")) %>%  #recode canopy positions from abbreviations
   arrange(type = factor(type, levels = c('Native', 'Naturalised', 'Invasive'))) %>%   #rearranges the categories in this order
   mutate(c_n = C/N)
@@ -89,7 +89,6 @@ cn_nns <- cn_trees %>%
 (cn_counts <- cn_trees %>%
     group_by(type) %>%
     summarise(unique_species = n_distinct(code)))
-
 
 #Exploration
 head(nns)
@@ -912,24 +911,23 @@ anosim(diss_matrix_chem, merged_data$code_two.x, permutations = 9999)
 
 
 #PCA in progress (badly and sadly) ---- 
-phys_subset <- nns %>% select(A, E, g, type)
-morph_subset <- nns %>% select(lma, ldcm, type)
+phys_subset <- trees %>% select(A, E, g, type)
+morph_subset <- trees %>% select(lma, ldcm, type)
+chem_subset1 <- cn_trees %>% select(c_n, type)
+chem_subset2 <- trees %>% select(chl, type)
 
-merged_data <- merge(cn_trees, nns, by = c("code", "canopy_pos"))
-chem_subset <- merged_data %>% select(c_n, chl, code_two.x)
+combined_tree_data <- merge(phys_subset, morph_subset, by = "type")
+combined_tree_data <- merge(combined_tree_data, chem_subset1, by = "type")
+combined_tree_data <- merge(combined_tree_data, chem_subset2, by = "type")
 
-combined_tree_data <- cbind(phys_subset, morph_subset)
 combined_tree_data$type <- factor(combined_tree_data$type)
 combined_tree_data_no_type <- combined_tree_data[, !names(combined_tree_data) %in% c("type")] #removes type as a variable; unnecesary for the PCA
 
 
 subset_data <- combined_tree_data[, c(1:3, 5:6)]
 scaled_data <- scale(subset_data)
-combined_tree_data_scaled[, c(1:3, 5:6)] <- scaled_data
 
-pca_result <- prcomp(combined_tree_data_scaled, scale. = TRUE)
-
-(pca_result <- prcomp(combined_tree_data_scaled, scale. = TRUE))
+(pca_result <- prcomp(scaled_data, scale. = TRUE))
 pca_df <- as.data.frame(pca_result$x)
 pca_df$type <- combined_tree_data$type  # Assuming 'type' is a column in combined_tree_data
 type_colors <- c("Native" = "#698B69", "Naturalised" = "#EEC900", "Invasive" = "#CD6090")
